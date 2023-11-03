c77adaf82c0e535b4adfaed2adf99008c2fdba8a
GIZsolt <zsoltikag2000@gmail.com>
2023-11-03 09:44:24 +0200

    Labor6

--- android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.tasty.recipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tasty.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        val TAG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+        val view = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(view.root)
+        val name = intent.extras?.getString("name")
+        view.textGreeting.text = "Hello $name"
     }
 }
---
+++ android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.tasty.recipesapp
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.tasty.recipesapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
